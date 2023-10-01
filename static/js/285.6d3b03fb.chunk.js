aa4e84dcd01a3bf96dca7ed33d220a4cfad7d31f
NikNas1405 <103371039+NikNas1405@users.noreply.github.com>
2023-10-01 05:20:35 +0000

    Deploying to gh-pages from @ NikNas1405/goit-react-hw-08-phonebook@9a591a7a9057a453ad0bd84fe4d888765312d8a0 🚀

--- static/js/285.6d3b03fb.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[285],{285:function(e,n,t){t.r(n),t.d(n,{default:function(){return i}});var o=t(9562),r=t(184);function i(){return(0,r.jsxs)("div",{children:[(0,r.jsx)("h2",{children:"Registration"}),(0,r.jsx)(o.B2,{})]})}}}]);
-//# sourceMappingURL=285.6d3b03fb.chunk.js.map
---
+++ static/js/38.493254d2.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[38],{4038:function(n,e,o){o.r(e),o.d(e,{default:function(){return c}});var t=o(3580),r=o(184);function c(){return(0,r.jsxs)("div",{children:[(0,r.jsx)("h2",{children:"Login"}),(0,r.jsx)(t.U0,{})]})}}}]);
+//# sourceMappingURL=38.493254d2.chunk.js.map
